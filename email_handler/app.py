40711a67721f8e0d46669825db54eea8bc263193
EMIT2019 <emilianoandreurbinazavala@gmail.com>
2025-08-08 00:41:20 -0600

    Refactor the send_email code to have a better email structure and added an .env for gmail loggin attributes

--- email_handler/app.py
+++ email_handler/app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from sender import EmailSender
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
 def test() -> str:
     return "<p>Hello World. Greetings from EMIT's computer.<p>"
+
+@app.route("/send")
+def send_mail() -> str:
+    host:str = os.getenv("EMAIL_HOST")
+    sender:str = os.getenv("EMAIL_SENDER")
+    password:str = os.getenv("EMAIL_PASSWORD")
+    port:int = int(os.getenv("EMAIL_PORT"))
+    addresse:list[str] = ["emilianoandreurbinazavala@gmail.com"]
+    email_content:dict = {
+        "subject":"This is a test from EMIT's PC.",
+        "body":"THIS IS JUST A TEST DO NOT MIND ABOUT IT"
+    }
+
+    new_sender:EmailSender = EmailSender(host, sender, password, port)
+    mail_status:bool = new_sender.send_email(addresse, email_content)
+    print(f"Mail Status: {mail_status}")    
+    return "<p>The mail was succesfully sent!!!<p>"
 
 if __name__ == "__main__":
     app.run()
--- email_handler/sender.py
+++ email_handler/sender.py
@@ -1,23 +1,28 @@
 import smtplib, ssl
-
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 class EmailSender():
-    def __init__(self):
-        pass
+    def __init__(self, host:str, sender:str, password:str, port:int, email_content:dict = None):
+        self.host = host
+        self.sender = sender
+        self.password = password
+        self.port = port
+        self.email_content = email_content
 
-    def send_email(self) -> bool:
+    def send_email(self, addresse:list[str], email_content:dict = None) -> bool:
         try:
-            port:int = 465
-            smtp_server:str = "smtp.gmail.com"
-            sender:str = "emilianoandreurbinazavala@gmail.com"
-            addresse:str = "emilianoandreurbinazavala@gmail.com"
-            password:str = ""
-            message:str = "Subject: This is only a test   \n this message was send from python"
+            message:MIMEMultipart = MIMEMultipart()
+            subject:str = email_content["subject"] if email_content is not None else self.email_content["subject"]
+            message["Subject"] = subject
+            message["From"] = self.sender
+            message["To"] = ", ".join(addresse)
+            body:str = email_content["body"] if email_content is not None else self.email_content["body"]
+            message.attach(MIMEText(body, 'plain'))
             context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-                server.login(sender, password)
-                server.sendmail(sender, addresse, message)
+            with smtplib.SMTP_SSL(self.host, self.port, context=context) as server:
+                server.login(self.sender, self.password)
+                server.sendmail(self.sender, addresse, message.as_string())
+            return True
         except Exception as ex:
             print(f"There was an error: {ex}")
-
-test = EmailSender()
-test.send_email()
+            return False
